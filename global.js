21b35f587e90dbc44b8346b8548c3577a9d20fc9
蒋海徽 <mxdhanri@gmail.com>
2017-03-13 00:39:48 +0800

    node_tools

--- global.js
+++ global.js
@@ -1,3 +1,40 @@
 /**
  * Created by jianghaihui on 2017/3/12.
  */
+/*var count = 10;
+console.log("total: %d",count);
+console.log("获取数据");
+console.trace();
+
+process.on('uncaughtException' ,function(code){
+
+    // 以下代码永远不会执行
+    setTimeout(function() {
+        console.log("该代码不会执行");
+    }, 0);
+
+    console.log('退出码为:', code);
+});
+
+console.log("程序执行结束");
+*/
+process.stdout.write("Hello World!" + "\n");
+
+// 通过参数读取
+process.argv.forEach(function(val, index, json) {
+    console.log(index + ': ' + val +':'+json);
+});
+
+console.log(process.execPath);
+
+console.log(process.platform);
+
+console.log('当前版本：' + process.version);
+
+console.log('当前目录：' + process.cwd());
+
+//内存适用情况
+console.log(process.memoryUsage());
+
+//node已经运行的秒数
+console.log(process.uptime());
--- global_fun.js
+++ global_fun.js
@@ -1,7 +1,7 @@
 /**
  * Created by jianghaihui on 2017/2/26.
  */
-
+//ECMAScript 就是由ECMA-262标准化的脚本语言的名称
 //全局变量，输出当前文件的绝对路径
 console.log(__filename);
 
@@ -11,3 +11,4 @@ console.log(__dirname);
 console.warn(['asdasdas']);
 
 
+console.log('asd%asd','99');
